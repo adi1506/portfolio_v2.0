3d28632d31466b548ed70c245ba395f13ac643f5
Aditya Chatterjee <adityachatterjee891@gmail.com>
2021-06-14 19:23:29 +0530

    Hero Section added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br/>
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non libero id libero scelerisque iaculis eu sit amet ex. Suspendisse sagittis dui libero, a cursus nisi vestibulum a. Cras turpis urna, facilisis id eros ac, posuere euismod leo. Pellentesque id maximus mi. Morbi mollis leo eu neque consectetur, ut congue.
+      </SectionText>
+      <Button onClick={() => window.location = 'mailto:adityachatterjee891@gmail.com'}>Hire Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
