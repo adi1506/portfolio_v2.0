c30cfff1a097ff1dd68cc4d6fce5a7d1eb449523
Aditya Chatterjee <adityachatterjee891@gmail.com>
2021-06-18 12:28:29 +0530

    Footer Section added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+    <LinkList>
+      <LinkColumn>
+        <LinkTitle>Email</LinkTitle>
+        <LinkItem href="mailto:adityachatterjee891@gmail.com">
+        adityachatterjee891@gmail.com
+        </LinkItem>
+      </LinkColumn>
+    </LinkList>
+    <SocialIconsContainer>
+      <CompanyContainer>
+        <Slogan>Innovating one project at a time</Slogan>
+      </CompanyContainer>
+      <SocialContainer>
+        <SocialIcons href="https://github.com/adi1506" target="_blank">
+          <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/aditya-c-840480140/" target="_blank">
+          <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://twitter.com/alpha__charlie" target="_blank">
+          <AiFillTwitterCircle size="3rem"/>
+        </SocialIcons>
+      </SocialContainer>
+    </SocialIconsContainer>
+  </FooterWrapper>
   );
 };
 
